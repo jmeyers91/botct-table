deeb52cec16206b0eda76556effe6c5b5ae5bf4c
James Meyers <james.meyers@emberex.com>
2020-05-05 18:22:07 -0700

    Add basic funtionality and create a build.

---
+++ build/precache-manifest.1d76bcdbb61fd85b74eb779fafcd23ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef381aed104d9d7e1bdec5154bd65cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0bddebc24b11ba97a50",
+    "url": "/static/css/main.26b6d568.chunk.css"
+  },
+  {
+    "revision": "ca823fcf84e2ccfeb5f4",
+    "url": "/static/js/2.62332971.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62332971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0bddebc24b11ba97a50",
+    "url": "/static/js/main.b6f3d22d.chunk.js"
+  },
+  {
+    "revision": "7786b6d5a5dd16fa5d3c",
+    "url": "/static/js/runtime-main.5e38c043.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,240 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect, useMemo } from "react";
+import { v4 as uuid } from "uuid";
+import "./App.css";
+
+const stateVersion = 1;
+const stateKey = `botct_state_${stateVersion}`;
+let initialState = localStorage.getItem(stateKey) || {
+  players: [],
+  showTable: false,
+};
+
+if (initialState && typeof initialState === "string") {
+  initialState = JSON.parse(initialState);
+}
 
 function App() {
+  const [showTable, setShowTable] = useState(initialState.showTable);
+  const [players, setPlayers] = useState(initialState.players);
+  const [canvas, setCanvas] = useState(null);
+  const canvasContext = useMemo(() => canvas && canvas.getContext("2d"), [
+    canvas,
+  ]);
+
+  const handleAddPlayer = useCallback(() => {
+    setPlayers((players) => [
+      ...players,
+      {
+        id: uuid(),
+        name: "",
+        dead: false,
+      },
+    ]);
+  }, []);
+
+  const handleRemovePlayer = useCallback((playerId) => {
+    setPlayers((players) => players.filter((player) => player.id !== playerId));
+  }, []);
+
+  const handlePlayerNameChange = useCallback((playerId, name) => {
+    setPlayers((players) =>
+      players.map((player) =>
+        player.id === playerId ? { ...player, name } : player
+      )
+    );
+  }, []);
+
+  const toggleShowTable = useCallback(
+    () => setShowTable((showTable) => !showTable),
+    []
+  );
+
+  const handleCanvasClick = useCallback(
+    (event) => {
+      if (!canvas) {
+        return;
+      }
+      const playerPositions = getPlayerPositions(canvas, players);
+      const mousePosition = getCanvasMousePosition(canvas, event);
+      const playerPositionsWithDistance = playerPositions
+        .map((playerPosition) => ({
+          playerPosition,
+          distance: getDistance(playerPosition, mousePosition),
+        }))
+        .sort((a, b) => a.distance - b.distance);
+
+      const [nearest] = playerPositionsWithDistance;
+
+      if (nearest && nearest.distance < 50) {
+        const { player: playerToKill } = nearest.playerPosition;
+
+        setPlayers(
+          players.map((player) =>
+            player.id === playerToKill.id
+              ? { ...player, dead: !player.dead }
+              : player
+          )
+        );
+      }
+    },
+    [canvas, players]
+  );
+
+  const handleExportTableClick = useCallback(() => {
+    if (!canvas) {
+      return;
+    }
+
+    const now = new Date();
+    const filename = `botct-table-${now.getHours()}_${now.getMinutes()}_${now.getSeconds()}.png`;
+    downloadCanvas(canvas, filename);
+  }, [canvas]);
+
+  useEffect(() => {
+    if (!canvas || !canvasContext) {
+      return;
+    }
+    renderTable(canvas, canvasContext, players);
+  }, [canvas, canvasContext, players]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      localStorage.setItem(stateKey, JSON.stringify({ players, showTable }));
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [players, showTable]);
+
+  if (showTable) {
+    return (
+      <div className="App">
+        <canvas
+          className="TableCanvas"
+          width={800}
+          height={800}
+          ref={setCanvas}
+          onClick={handleCanvasClick}
+        />
+        <button onClick={handleExportTableClick}>Export</button>
+        <button onClick={toggleShowTable}>Back to Player List</button>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className="PlayerList">
+        {players.map((player) => (
+          <li key={player.id} className="PlayerListItem">
+            <input
+              className="PlayerNameInput"
+              value={player.name}
+              onChange={(event) =>
+                handlePlayerNameChange(player.id, event.currentTarget.value)
+              }
+            />
+            <button
+              className="PlayerDeleteButton"
+              onClick={() => handleRemovePlayer(player.id)}
+            >
+              {"\u2715"}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleAddPlayer}>Add Player</button>
+      <button onClick={toggleShowTable}>View Table</button>
     </div>
   );
+}
+
+function renderTable(canvas, context, players) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "white";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "black";
+  const tableCircle = getTableCircle(canvas);
+  const playerPositions = getPlayerPositions(canvas, players);
+  const deadXWidth = 25;
+
+  context.beginPath();
+  context.arc(tableCircle.x, tableCircle.y, tableCircle.radius, 0, Math.PI * 2);
+  context.stroke();
+
+  context.font = "16px Arial";
+  context.textAlign = "center";
+
+  for (const { x, y, player } of playerPositions) {
+    context.fillText(player.name, x, y + 8, 100);
+    context.stroke();
+
+    if (player.dead) {
+      context.beginPath();
+      context.moveTo(x - deadXWidth, y - deadXWidth);
+      context.lineTo(x + deadXWidth, y + deadXWidth);
+      context.stroke();
+
+      context.beginPath();
+      context.moveTo(x - deadXWidth, y + deadXWidth);
+      context.lineTo(x + deadXWidth, y - deadXWidth);
+      context.stroke();
+    }
+  }
+}
+
+function getTableCircle(canvas) {
+  const { width, height } = canvas;
+  const centerX = width / 2;
+  const centerY = height / 2;
+  const circleRadius = Math.min(width, height) / 2 - 200;
+
+  return {
+    x: centerX,
+    y: centerY,
+    radius: circleRadius,
+  };
+}
+
+function getPlayerPositions(canvas, players) {
+  const tableCircle = getTableCircle(canvas);
+  const playerCount = players.length;
+
+  return players.map((player, i) => ({
+    player,
+    x:
+      tableCircle.x +
+      (tableCircle.radius + 50) * Math.cos(((Math.PI * 2) / playerCount) * i),
+    y:
+      tableCircle.y +
+      (tableCircle.radius + 50) * Math.sin(((Math.PI * 2) / playerCount) * i),
+  }));
+}
+
+function getCanvasMousePosition(canvas, event) {
+  const { left, top, width, height } = canvas.getBoundingClientRect();
+  const scaleX = canvas.width / width;
+  const scaleY = canvas.height / height;
+
+  return {
+    x: (event.clientX - left) * scaleX,
+    y: (event.clientY - top) * scaleY,
+  };
+}
+
+function getDistance(a, b) {
+  const dx = b.x - a.x;
+  const dy = b.y - a.y;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+function downloadCanvas(canvas, fileName) {
+  const link = document.createElement("a");
+  link.setAttribute("download", fileName);
+  link.setAttribute(
+    "href",
+    canvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
+  );
+  link.click();
 }
 
 export default App;
